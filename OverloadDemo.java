e0b6d4037fdb213b19e844ccd6c8af27b22f0338
Christian Jarrell <christian.j.jarrell@gmail.com>
2026-05-17 16:51:14 -0400

    Initial setup: add OverloadDemo class with comment

---
+++ OverloadDemo.java
@@ -0,0 +1,14 @@
+/*
+ * This program demonstrates method overloading in Java.
+ * This multiUse method behaves differently depending on how many arguments are passed:
+ *  - One integer: prints numbers from 1 to that value
+ *  - Two integers: calculates and prints their average
+ *  - Three integeers: finds a prints the max value
+ */
+
+public class OverloadDemo {
+
+    public static void main(String[] args) {
+        // Tests will be added here
+    }
+}
